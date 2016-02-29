c73bedceeb46be304c1520f4834f6f4af25cd8cb
Gleb Poljakov <gleb.poljakov@gmail.com>
2016-02-29 13:02:27 +0300

    v0.05

--- ice-rec.sh
+++ ice-rec.sh
@@ -1,63 +1,105 @@
-#!/bin/sh
+#!/bin/sh -x
 #
 # 2008_01_15
 # Gleb GByte Poljakov
-# ver: 0.04
+ver="0.05"
+name="Ice-Rec"
+pname="${name} v${ver}"
 #
 
+	msg (){
+    
+		# Procedure for writing debug messages
+		# params: $1 - message, $2 - level of the message (error, warning, notify)
+    
+		[ $2 -le $debug ] && logger -t "$log_tag" "$1"
+    }
+
+    stop (){
+    
+		# Procedure for stoping wget-process    
+
+		set +bm;
+
+		msg "Get stop signal. Stoped." 0
+
+		msg "Kill all child proceses" 1
+
+		for i in `ps h --ppid ${wget_pid} -o pid`
+		do
+			kill -9 $i > /dev/null;			
+		done;
+
+		kill -9 $wget_pid > /dev/null;
+    }
+    
+	usage () {
+    	
+    	#
+		# Procedure.
+    	# shows usage info and exit
+    	#
+
+    	echo >&2 \
+		"usage: $0 -u URL -o DIR [-d debug_level]"
+		exit 1;
+    }
+
+#System vars
+    log_tag="${pname} [${$}]"
+    debug="1"					#debug level
+
+#parse command line
 # -o $FILE
 # -u #URL
 # -t $duration
 # -d $debug_level
-
-# Procedure for writing debug messages
-# $1 - message, $2 - level of the message (error, warning, notify)
-    msg (){
-	[ $2 -le $debug ] && logger -t "$log_tag" "$1"
-    }
-
-#Procedure for stoping wget-process    
-    stop (){
-	set +bm;
-	kill -9 $wget_pid > /dev/null;
-    }
-
-#System vars
-    log_tag="Ice-Rec v0.03"
-    debug="1"					#debug level
-
-#parse command line
     while getopts t:d:o:u: opt
     do
-	case "$opt" in
-	    t)	duration="$OPTARG";;
-	    d)	debug="$OPTARG";;
-	    o)	FILE="$OPTARG";;
-	    u)	URL="$OPTARG";;
-	    \?)				#unknown
-		echo >&2 \
-		"usage: $0 -t time_duration -u URL -o DIR [-d debug_level]"
-		exit 1;;
-	esac
+		case "$opt" in
+	    	t)	duration="$OPTARG";;
+			d)	debug="$OPTARG";;
+			o)	FILE="$OPTARG";;
+			u)	URL="$OPTARG";;
+			\?)	usage;;
+		esac
     done
     shift `expr $OPTIND - 1`
 
-#Check for correct variables values:
-    if [ -z "$duration" ]; then
-	msg 'Expected -t time_duration!' 0
-	exit 1;
-    fi;
+# Check for correct variable values:
+    [ -z "$duration" ] && usage;
+	
+    [ -z "${FILE}" ] && usage;
+    [ -z "${URL}" ] && usage;
 
-#    [ ! -d ${DIR} ] && mkdir -p ${DIR}
-#    cd ${DIR}
-
-#Run
-    wget -t 0 -q --no-proxy ${URL} -O ${FILE} &
-    wget_pid="$!"
-
+#
+# Run
+#
     sleep $duration &
     sleep_pid="$!"
 
-set -bm
-trap 'stop' TERM INT CHLD
-wait $sleep_pid
+	msg "Run:" 0
+	msg "   URL: ${URL}" 0
+	msg "   FILE: ${FILE}" 0
+
+	i=0;
+		while true;
+		do
+			wget -q --retry-connrefused --no-proxy ${URL} -O ${FILE%.???}.${i}.${FILE##*.}
+
+			sleep 5;
+
+			i=$(($i+1));
+			msg "Restart wget. Retry ${i}" 0;
+		done &
+
+	wget_pid="$!";
+
+#	set -bm
+#	trap 'stop' TERM INT CHLD # EXIT QUIT HUP
+
+	wait $sleep_pid
+	stop;
+
+	exit 0;
+
